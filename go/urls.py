88b84f7b5dba1d6a963a0f052e7eafc322cdabab
412662903 <412662903@qq.com>
2018-04-05 21:10:18 +0800

    init

--- go/urls.py
+++ go/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url,include
+from go import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    url(r'^go/', include('go.urls')),
+    url(r'^index/', views.user_list),
 ]
--- mydjango/settings.py
+++ mydjango/settings.py
@@ -115,7 +115,12 @@ USE_L10N = True
 USE_TZ = True
 
 
+
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/2.0/howto/static-files/
+# https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+# 静态文件路径
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
