32ce5910288b3e7394bbb0bc11b5ee23ef4b4609
412662903 <412662903@qq.com>
2018-04-06 19:29:47 +0800

    init

--- mydjango/settings.py
+++ mydjango/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'mydjango.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': '',
+        'NAME': 'communication',
         'USER': '',
         'PASSWORD': '',
         'HOST': '',
